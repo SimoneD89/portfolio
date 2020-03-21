912427672376cd4af82be7d2bcf1ee90d0606bd7
Simone <40129272+SimoneD89@users.noreply.github.com>
2020-03-21 18:10:49 +0100

    minor commit

--- Coronavirus/coronavirus.py
+++ Coronavirus/coronavirus.py
@@ -3,15 +3,11 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
 import numpy as np
-from scipy.optimize import curve_fit
+from sklearn.linear_model import LinearRegression
 from pandas.plotting import register_matplotlib_converters
 import seaborn as sns
 sns.set()
 register_matplotlib_converters()
-
-
-def lin(t, tau, t0, c):
-    return (t-t0)/tau + c
 
 
 def spacing(n):
@@ -38,23 +34,23 @@ country_ds["color"] = [
     "purple", "brown", "green", "orange", "blue", "red", "black", "crimson",
     "darkblue"
 ]
+country_ds.set_index("name", inplace=True)
 
-for i in country_ds.index:
+for name in country_ds.index:
     kwargs = {"delimiter": ";", "index_col": "data", "parse_dates": True}
-    if country_ds["date_format"].iloc[i] is not None:
+    if country_ds["date_format"].loc[name] is not None:
         kwargs["date_parser"] = lambda x: pd.to_datetime(
-                x, format=country_ds["date_format"].iloc[i])
+                x, format=country_ds["date_format"].loc[name])
 
-    df = pd.read_csv("datasets/" + country_ds["filename"].iloc[i], **kwargs)
-    country_ds["tot_count"].iloc[i] = df["count"].iloc[-1]
-    country_ds["tot_death"].iloc[i] = df["death"].iloc[-1]
+    df = pd.read_csv("datasets/" + country_ds["filename"].loc[name], **kwargs)
+    country_ds.loc[name, "tot_count"] = df["count"].iloc[-1]
+    country_ds.loc[name, "tot_death"] = df["death"].iloc[-1]
 
 country_ds["tot_count"] = country_ds["tot_count"].astype(int)
 country_ds["tot_death"] = country_ds["tot_death"].astype(int)
 country_ds["lethality"] = country_ds["tot_death"]/country_ds["tot_count"]*100
 country_ds["last_update"] = country_ds["tot_death"].astype("datetime64[ns]")
 country_ds.sort_values(by=["tot_count"], ascending=False, inplace=True)
-country_ds.reset_index(drop=True, inplace=True)
 
 plt.rcParams.update({"font.size": 12})
 
@@ -63,21 +59,18 @@ f, (ax1, ax2) = plt.subplots(2, 1, sharex=True,
                              gridspec_kw={"height_ratios": [2, 1]})
 f.subplots_adjust(hspace=0)
 
-for i in country_ds.index:
+model = LinearRegression(fit_intercept=True)
+for name in country_ds.index:
     kwargs = {"delimiter": ";", "index_col": "data", "parse_dates": True}
-    if country_ds["date_format"].iloc[i] is not None:
+    if country_ds["date_format"].loc[name] is not None:
         kwargs["date_parser"] = lambda x: pd.to_datetime(
-                x, format=country_ds["date_format"].iloc[i])
-    df = pd.read_csv("datasets/" + country_ds["filename"].iloc[i], **kwargs)
+                x, format=country_ds["date_format"].loc[name])
+    df = pd.read_csv("datasets/" + country_ds["filename"].loc[name], **kwargs)
 
     t = mdates.date2num(df.index)
-    country_ds["last_update"].iloc[i] = df.index[-1]
+    country_ds.loc[name, "last_update"] = df.index[-1]
 
-    param = [1, t[-7], 0]
-
-    param, _ = curve_fit(lin, t[-7:],
-                         np.log2(df["count"].values[-7:]),
-                         p0=param)
+    model.fit(t[-7:, np.newaxis], np.log2(df["count"].values[-7:]))
 
     t1 = np.linspace(t[-7:].min() - 0.5, t.max() + 0.25, 100)
     t2 = np.linspace(t[-1] - 15, t[-7:].min() - 0.5, 1000)
@@ -86,24 +79,24 @@ for i in country_ds.index:
 
     label = (r"%s: $\tau \simeq$ %.1f d, " +
              r"tot = %s ($\dag\,$%s $\cdot$ %.1f%%)") % (
-        country_ds["name"].iloc[i],
-        param[0],
-        spacing(country_ds["tot_count"].iloc[i]),
-        spacing(country_ds["tot_death"].iloc[i]),
-        country_ds["lethality"].iloc[i]
+        name,
+        1/model.coef_,
+        spacing(country_ds["tot_count"].loc[name]),
+        spacing(country_ds["tot_death"].loc[name]),
+        country_ds["lethality"].loc[name]
     )
     ax1.plot(df.index, df["count"].values, ".",
-             color=country_ds["color"].iloc[i], label=label, markersize=8)
-    ax1.plot(d1, 2**(lin(t1, *param)), "-.", alpha=0.6,
-             color=country_ds["color"].iloc[i], linewidth=0.8)
-    ax1.plot(d2, 2**(lin(t2, *param)), "-.", alpha=0.3,
-             color=country_ds["color"].iloc[i], linewidth=0.8)
+             color=country_ds["color"].loc[name], label=label, markersize=8)
+    ax1.plot(d1, 2**(model.predict(t1[:, np.newaxis])), "-.", alpha=0.6,
+             color=country_ds["color"].loc[name], linewidth=0.8)
+    ax1.plot(d2, 2**(model.predict(t2[:, np.newaxis])), "-.", alpha=0.3,
+             color=country_ds["color"].loc[name], linewidth=0.8)
 
     df["death"].fillna(0, inplace=True)
     ax2.plot(df.index, (100*df["death"]/df["count"]).values, ".",
-             color=country_ds["color"].iloc[i], markersize=8)
+             color=country_ds["color"].loc[name], markersize=8)
     ax2.plot(df.index, (100*df["death"]/df["count"]).values, "-.",
-             color=country_ds["color"].iloc[i], alpha=0.3, linewidth=0.8)
+             color=country_ds["color"].loc[name], alpha=0.3, linewidth=0.8)
 
 ax1.text(0.01, 0.99, r"$\bf{Data Source:}$ " +
                      r"Wikipedia $\cdot$ 2019–20_coronavirus_pandemic",
